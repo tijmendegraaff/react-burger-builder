433fa26f32a55d4f45ec463d3a1937875d53a877
Tijmen de Graaff <tijmen76@gmail.com>
2017-11-19 17:06:28 +0100

    add logic to toggle off sidedrawer

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,17 +1,31 @@
-import React from "react"
+import React, { Component } from "react"
 
 import Aux from "../../hoc/Aux"
 import Toolbar from "../Navigation/Toolbar/Toolbar"
 import SideDrawer from "../Navigation/SideDrawer/SideDrawer"
 import classes from "./Layout.css"
 
-const layout = props => (
-    <Aux>
-        <Toolbar />
-        <SideDrawer />
-        <div>Toolbar, SideDrawer, Backdrop</div>
-        <main className={classes.Content}>{props.children}</main>
-    </Aux>
-)
+class Layout extends Component {
+    state = {
+        showSideDrawer: true
+    }
+    sideDrawerClosedHandler = () => {
+        this.setState({ showSideDrawer: false })
+    }
 
-export default layout
+    render() {
+        return (
+            <Aux>
+                <Toolbar />
+                <SideDrawer
+                    closed={this.sideDrawerClosedHandler}
+                    open={this.state.showSideDrawer}
+                />
+                <div>Toolbar, SideDrawer, Backdrop</div>
+                <main className={classes.Content}>{this.props.children}</main>
+            </Aux>
+        )
+    }
+}
+
+export default Layout
